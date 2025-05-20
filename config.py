4b2a257bd72c1fbdc96ba9a658e40e0beb9b4f49
Manasseh Amoako <xasongab@gmail.com>
2025-05-20 14:12:28 +0100

    Updated bot.py, .env, and config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-API_ID = 21905803
-API_HASH = "27b4f9f6af164b9f6f12cfca5e9bbc0e"
-BOT_TOKEN = "7888223326:AAHBumdGJtdndD0hVMfcZUtby7km9Zdv-eE"
+import os
+
+API_ID = int(os.environ.get("API_ID", 21905803))
+API_HASH = os.environ.get("API_HASH", "27b4f9f6af164b9f6f12cfca5e9bb0e0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7888223326:AAHBunmdGJtdndDOhVMfcZUtby7kmZdv-eE")
